e0311aa69aff5c22bf916fc0f28c5cecfa650e76
ahza64 <ahza64@yahoo.com>
2018-02-07 23:43:16 -0800

    add process.env.PORT

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express');
 
 var app = express();
 
-var port = 3232;
 
 app.use(express.static('public'));
 
+var port = process.env.PORT || 3232;
 app.listen(port, function () {
   console.log('node express frontend up on port' + port);
 });

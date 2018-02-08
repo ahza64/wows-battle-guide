5e3fe8c820d0f65c175dec29a194984a296338db
ahza64 <ahza64@yahoo.com>
2018-02-08 02:45:42 -0800

    add button

--- public/components/nav/nav.jsx
+++ public/components/nav/nav.jsx
@@ -4,19 +4,27 @@ import React from 'react';
 // Styles
 import Nav from 'react-bootstrap/lib/Nav';
 import Navbar from 'react-bootstrap/lib/Navbar';
+import Button from 'react-bootstrap/lib/Button';
 
 export default class ParentNav extends React.Component {
+
+  handleSubmitLogin(event) {
+    console.log("you clicked a button that does nothing");
+  }
+
   render () {
     return (
       <Navbar>
         <Navbar.Header>
           <Navbar.Brand>
-            <div>World of Warships Battle Guide</div>
+            <div>World of Warships Battle Guide (interm version 0)</div>
           </Navbar.Brand>
 
         </Navbar.Header>
         <Navbar.Collapse>
-
+          <Navbar.Form pullRight>
+            <Button type="submit" onClick={event => this.handleSubmitLogin(event)}>Doesn't do anything yet</Button>
+          </Navbar.Form>
         </Navbar.Collapse>
       </Navbar>
     )

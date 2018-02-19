7b8529f08f1c2453a841d4ce302a1127dda0e6e8
ahza64 <ahza64@yahoo.com>
2018-02-19 14:34:47 -0800

    fix url/refresh bug

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.get(['/'], function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 app.get(['/*'], function (req, res) {
- res.sendFile(__dirname + '/public/index.html');
+  res.redirect(req.baseUrl + '/');
 });
 
 var port = process.env.PORT || 3232;
